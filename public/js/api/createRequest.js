559f6eb00d1cb6d5383e180d32587db10771c3d5
Neto test <nikolajeliseev834@gmail.com>
2025-03-31 17:15:22 +0300

    добавил условия

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,13 +3,27 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
-  const { url, method = 'GET' } = options;
+  const { url, method = "GET", data } = options;
+
   let requestUrl = url;
   const formData = new FormData();
 
-  if (method === 'GET') {
+  if (method === "GET") {
     if (data) {
-
+      const params = new URLSearchParams();
+      for (const key in data) {
+        params.append(key, data[key]);
+      }
+      requestUrl += `?${params.toString()}`;
     }
-  } 
+  }
+
+  if (!method === "GET") {
+    if (data) {
+      for (const key in data) {
+        formData.append(key, data[key]);
+      }
+    }
+  }
+
 };
