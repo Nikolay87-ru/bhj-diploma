54fc9393026af4669f9d28847b46a2455a5123ab
Neto test <nikolajeliseev834@gmail.com>
2025-03-31 16:55:17 +0300

    initial commit

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,13 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  const { url, method = 'GET' } = options;
+  let requestUrl = url;
+  const formData = new FormData();
 
+  if (method === 'GET') {
+    if (data) {
+
+    }
+  } 
 };
