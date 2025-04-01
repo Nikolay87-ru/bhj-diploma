86af60de871e844db78356f233b1710330be3493
Neto test <nikolajeliseev834@gmail.com>
2025-04-01 15:55:36 +0300

    Добавил метод get в класс Account + правки других модулях

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -1,14 +1,6 @@
-/**
- * Класс Entity - базовый для взаимодействия с сервером.
- * Имеет свойство URL, равно пустой строке.
- * */
 class Entity {
   static URL = "";
-  /**
-   * Запрашивает с сервера список данных.
-   * Это могут быть счета или доходы/расходы
-   * (в зависимости от того, что наследуется от Entity)
-   * */
+
   static list(data, callback) {
     createRequest({
       url: this.URL,
@@ -18,11 +10,6 @@ class Entity {
     });
   }
 
-  /**
-   * Создаёт счёт или доход/расход с помощью запроса
-   * на сервер. (в зависимости от того,
-   * что наследуется от Entity)
-   * */
   static create(data, callback) {
     createRequest({
       url: this.URL,
@@ -32,10 +19,6 @@ class Entity {
     });
   }
 
-  /**
-   * Удаляет информацию о счёте или доходе/расходе
-   * (в зависимости от того, что наследуется от Entity)
-   * */
   static remove(data, callback) {
     createRequest({
       url: this.URL,
