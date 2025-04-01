515f1fa635d726295d9af22e165500e873084ab9
Neto test <nikolajeliseev834@gmail.com>
2025-04-01 12:08:20 +0300

    Добавил запрос с сервера для класса Entity

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -3,13 +3,19 @@
  * Имеет свойство URL, равно пустой строке.
  * */
 class Entity {
+  URL = "";
   /**
    * Запрашивает с сервера список данных.
    * Это могут быть счета или доходы/расходы
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static list(data, callback){
-
+  static list(data, callback) {
+    createRequest({
+      url: this.URL,
+      method: 'GET',
+      data: data,
+      callback: callback
+    });
   }
 
   /**
@@ -17,15 +23,11 @@ class Entity {
    * на сервер. (в зависимости от того,
    * что наследуется от Entity)
    * */
-  static create(data, callback) {
-
-  }
+  static create(data, callback) {}
 
   /**
    * Удаляет информацию о счёте или доходе/расходе
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static remove(data, callback ) {
-
-  }
+  static remove(data, callback) {}
 }
