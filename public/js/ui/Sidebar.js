c6e06a52b67fe1b029986c1a0abf19a23161c290
Neto test <nikolajeliseev834@gmail.com>
2025-04-02 19:08:10 +0300

    Удалил лишние комментарии

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -1,22 +1,9 @@
-/**
- * Класс Sidebar отвечает за работу боковой колонки:
- * кнопки скрытия/показа колонки в мобильной версии сайта
- * и за кнопки меню
- * */
 class Sidebar {
-  /**
-   * Запускает initAuthLinks и initToggleButton
-   * */
   static init() {
     this.initAuthLinks();
     this.initToggleButton();
   }
 
-  /**
-   * Отвечает за скрытие/показа боковой колонки:
-   * переключает два класса для body: sidebar-open и sidebar-collapse
-   * при нажатии на кнопку .sidebar-toggle
-   * */
   static initToggleButton() {
     document.addEventListener("DOMContentLoaded", () => {
       const sidebarToggle = document.querySelector(".sidebar-toggle");
@@ -48,5 +35,7 @@ class Sidebar {
    * При нажатии на кнопку выхода вызывает User.logout и по успешному
    * выходу устанавливает App.setState( 'init' )
    * */
-  static initAuthLinks() {}
+  static initAuthLinks() {
+    
+  }
 }
