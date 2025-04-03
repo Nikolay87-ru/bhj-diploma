218ba6de844fd46799677ca5097093b70a146d37
Neto test <nikolajeliseev834@gmail.com>
2025-04-03 18:44:59 +0300

    предварительная логика для модуля AsyncForm

--- public/js/ui/forms/AsyncForm.js
+++ public/js/ui/forms/AsyncForm.js
@@ -1,17 +1,4 @@
-/**
- * Класс AsyncForm управляет всеми формами
- * приложения, которые не должны быть отправлены с
- * перезагрузкой страницы. Вместо этого данные
- * с таких форм собираются и передаются в метод onSubmit
- * для последующей обработки
- * */
 class AsyncForm {
-  /**
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * Сохраняет переданный элемент и регистрирует события
-   * через registerEvents()
-   * */
   constructor(element) {
     this.element = element;
     this.registerEvents();
@@ -21,26 +8,29 @@ class AsyncForm {
     }
   }
 
-  /**
-   * Необходимо запретить отправку формы и в момент отправки
-   * вызывает метод submit()
-   * */
-  registerEvents() {}
+  registerEvents() {
+    this.element.addEventListener("submit", (event) => {
+      event.preventDefault();
+      this.submit();
+    });
+  }
 
-  /**
-   * Преобразует данные формы в объект вида
-   * {
-   *  'название поля формы 1': 'значение поля формы 1',
-   *  'название поля формы 2': 'значение поля формы 2'
-   * }
-   * */
-  getData() {}
+  getData() {
+    const formData = new FormData(this.element);
+    const entries = formData.entries();
+    const data = {};
+
+    for (let [name, value] of entries) {
+      data[name] = value;
+    }
+
+    return data;
+  }
 
   onSubmit(options) {}
 
-  /**
-   * Вызывает метод onSubmit и передаёт туда
-   * данные, полученные из метода getData()
-   * */
-  submit() {}
+  submit() {
+    const formData = this.getData();
+    this.onSubmit(formData);
+  }
 }
