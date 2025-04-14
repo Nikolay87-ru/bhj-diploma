88884b884ae02112749a17acf3834283baff1f83
Neto test <nikolajeliseev834@gmail.com>
2025-04-14 13:26:34 +0300

    исправления в форме выхода

--- public/js/ui/forms/LogoutForm.js
+++ public/js/ui/forms/LogoutForm.js
@@ -1,10 +1,11 @@
 class LogoutForm extends AsyncForm {
-  onSubmit(data) {
-    User.logout(data, (error, response) => {
-      if (error || !response?.success) {
+  onSubmit() {
+    User.logout((error) => {
+      if (error) {
+        console.error("Ошибка выхода:", error);
         return;
       }
-
+      
       this.element.reset();
       App.setState("init");
       App.getModal("logout").close();
